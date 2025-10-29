2e1c828b2283321609eaff93314e7c93ab155e43
Schwarz Kruppzo <schwarzkruppzo@gmail.com>
2025-10-29 15:13:54 +0300

    setup shader parameters on first draw

--- helix/plugins/visuals/effects/cl_chromatica.lua
+++ helix/plugins/visuals/effects/cl_chromatica.lua
@@ -1,12 +1,18 @@
 local SetMaterial, DrawScreenQuad, UpdateScreenEffectTexture = render.SetMaterial, render.DrawScreenQuad, render.UpdateScreenEffectTexture
 
 local chroma = Material("effects/shaders/autonomous_chroma")
-chroma:SetFloat("$c0_x", 0.020)
-chroma:SetFloat("$c0_y", 1)
+local firstDraw
 
 hook.Add("RenderScreenspaceEffects", "autonomous.chroma", function()
 	UpdateScreenEffectTexture()
 
+	if !firstDraw then
+		chroma:SetFloat("$c0_x", 0.020)
+		chroma:SetFloat("$c0_y", 1)
+
+		firstDraw = true
+	end
+	
 	SetMaterial(chroma)
 	DrawScreenQuad()
 end)
--- helix/plugins/visuals/effects/cl_filmgrain.lua
+++ helix/plugins/visuals/effects/cl_filmgrain.lua
@@ -1,14 +1,20 @@
 local SetMaterial, DrawScreenQuad, UpdateScreenEffectTexture = render.SetMaterial, render.DrawScreenQuad, render.UpdateScreenEffectTexture
 
 local filmgrain = Material("effects/shaders/autonomous_filmgrain")
-filmgrain:SetFloat("$c0_y", 2) //BLENDMODE
-filmgrain:SetFloat("$c0_z", 0.5) //SPEED
-filmgrain:SetFloat("$c0_w", 0.095) //INTENSITY
-filmgrain:SetFloat("$c1_x", 0) //MEAN
-filmgrain:SetFloat("$c1_y", 0.4) //VARIANCE
+local firstDraw
 
 hook.Add("RenderScreenspaceEffects", "autonomous.filmgrain", function()
 	UpdateScreenEffectTexture()
+
+	if !firstDraw then
+		filmgrain:SetFloat("$c0_y", 2) //BLENDMODE
+		filmgrain:SetFloat("$c0_z", 0.5) //SPEED
+		filmgrain:SetFloat("$c0_w", 0.095) //INTENSITY
+		filmgrain:SetFloat("$c1_x", 0) //MEAN
+		filmgrain:SetFloat("$c1_y", 0.4) //VARIANCE
+
+		firstDraw = true
+	end
 	
 	SetMaterial(filmgrain)
 	DrawScreenQuad()
