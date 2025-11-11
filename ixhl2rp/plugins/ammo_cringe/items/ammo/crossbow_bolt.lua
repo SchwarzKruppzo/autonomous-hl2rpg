7550e6c42620ea5b0529c17e51e3f4d9b30e59a9
aw-c <shitikov_1910@mail.ru>
2025-11-11 19:59:06 +0300

    crossbow things

--- ixhl2rp/plugins/ammo_cringe/items/ammo/crossbow_bolt.lua
+++ ixhl2rp/plugins/ammo_cringe/items/ammo/crossbow_bolt.lua
@@ -1,12 +1,14 @@
-ITEM.name = "Арматурные болты"
-ITEM.description = "Куски арматуры длиной 12 дюймов, предназначенные в качестве боеприпасов для самодельных арбалетов раскалённого типа."
+ITEM.name = "Арматурный болт"
+ITEM.description = "Кусок арматуры длиной 12 дюймов, предназначен для питания арбалетов раскалённого типа."
+ITEM.rarity = 1
 ITEM.model = "models/items/crossbowrounds.mdl"
-ITEM.width = 3
-ITEM.height = 1
+ITEM.width = 1
+ITEM.height = 4
+ITEM.cost = 350
 ITEM.iconCam = {
-	pos = Vector(-4.2456526756287, 333.95260620117, 88.609954833984),
-	ang = Angle(14.859113693237, 270.72836303711, 0),
-	fov = 4.1331167170138,
+	pos = Vector(-1.2007131576538, -0.011004903353751, 199.99633789063),
+	ang = Angle(89.656005859375, 0.52512621879578, 0),
+	fov = 1.6296793773914,
 }
-ITEM.max_stack = 16
+ITEM.max_stack = 4
 ITEM.default_stack = 1
---
+++ ixhl2rp/plugins/recipes_crafting/crafting/8/gun_scope_calibrated.lua
@@ -0,0 +1,12 @@
+RECIPE.name = "Откалиброванный прицел"
+RECIPE.category = "Калибровка"
+RECIPE.requirements = {
+	gun_scope = 1
+}
+RECIPE.results = {
+	gun_scope_calibrated = 1
+}
+RECIPE.skill = {"guns", 8}
+RECIPE.station = "station_tokar"
+RECIPE.tools = {"tool_screw"}
+RECIPE.xp = 0
