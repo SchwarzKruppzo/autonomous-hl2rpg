6cf1684f499d6d000ac9444204e138442d5688e2
aw-c <shitikov_1910@mail.ru>
2025-12-06 23:39:14 +0300

    feature: portable deploy scanner

--- ixhl2rp/plugins/combinescanners/items/combine_scanner.lua
+++ ixhl2rp/plugins/combinescanners/items/combine_scanner.lua
@@ -1,3 +1,5 @@
+local PLUGIN = PLUGIN
+
 ITEM.name = "Автономный сканнер Альянса"
 ITEM.description = "Сложенный по направляющим сканнер Альянса, разъём OBD-II позволяет связаться с ним через CAN протокол для дальнейшего программирования. После активации поддерживает только удалённое управление по защищённой сети."
 ITEM.model = "models/Combine_Scanner.mdl"
@@ -8,6 +10,16 @@ ITEM.iconCam = {
 	pos = Vector(65.359474182129, -22.143999099731, -10.739000320435),
 	ang = Angle(-10, 160.00001525879, 1.457099642721e-05),
 	fov = 24.117647058824,
+}
+
+ITEM.functions.activateScanner = {
+	name = "Активировать",
+	OnRun = function(item)
+		return PLUGIN:ActivateScannerAsItem(item)
+	end,
+	OnCanRun = function(item)
+		return !!item.entity
+	end
 }
 
 ITEM.contraband = true
