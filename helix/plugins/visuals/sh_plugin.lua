e688d255533106b166fe89778c64a09cbe4365c7
Schwarz Kruppzo <schwarzkruppzo@gmail.com>
2026-01-08 13:03:44 +0300

    visuals: removed unused lang entries

--- helix/plugins/visuals/sh_plugin.lua
+++ helix/plugins/visuals/sh_plugin.lua
@@ -2,18 +2,4 @@ PLUGIN.name = "Enhanced Visuals"
 PLUGIN.author = ""
 PLUGIN.description = ""
 
-ix.lang.AddTable("russian", {
-	optEnableLensFlare = "Включить блики света",
-	optdEnableLensFlare = "Включает реалистичные блики от источников света на экране.",
-
-	optEnableDirtyLens = "Включить следы пыли",
-	optdEnableDirtyLens = "Включает эффект постобработки загрязнённого экрана у источников света.",
-
-	optEnableChromaticAberration = "Включить хроматическую аберрацию",
-	optdEnableChromaticAberration = "Включает эффект постобработки хроматических аберраций, делая цвета более насыщенными и приятными.",
-
-	optEnableFilmGrain = "Включить зернистость",
-	optdEnableFilmGrain = "Включает эффект зернистости экрана для большего погружения.",
-})
-
 ix.util.IncludeDir(PLUGIN.folder .. "/effects", true)
