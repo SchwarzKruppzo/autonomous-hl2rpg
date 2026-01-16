c81e8ed5c4652f0b5a16fcdfe01c2d4b1453f0de
Schwarz Kruppzo <schwarzkruppzo@gmail.com>
2026-01-16 11:33:26 +0300

    schema: french localization update by Edel & Schlieffen

--- ixhl2rp/schema/languages/french.lua
+++ ixhl2rp/schema/languages/french.lua
@@ -1,6 +1,10 @@
 ix.Locale:Build("fr")
 
 
+-- FRENCH TRANSLATION
+-- @a.version, @dervonschlieffen
+
+
 schemaName					= "AUTONOMOUS"
 tying						= "Cette personne est en train d'être attachée..."
 unTying						= "Cette personne est en train d'être détachée..."
@@ -26,6 +30,15 @@ radioRequired				= "Vous avez besoin d'une radio pour faire ça."
 radioAlreadyOn				= "Vous avez déjà une radio qui est allumée !"
 economy						= "Économie"
 
+option.colorSaturation			= "Saturation des couleurs"
+option.colorSaturation.desc		= "Saturation des couleurs de l'écran."
+option.colorModify				= "Modifier la couleur"
+option.colorModify.desc			= "Active ou désactive la modification de la couleur lors du rendu."
+
+	
+chatRDevice  				= "Utilise le dispositif de demande..."
+chatRadioning  				= "Parle à la radio..."
+
 -- combine display messages
 cViewData					= "Téléchargement des données du profil citoyen..."
 cViewDataExpired			= "ERREUR ! Échec du téléchargement des données révisées du profil citoyen..."
@@ -48,3 +61,33 @@ cLosingContact				= "Téléchargement des informations du contact radio perdu...
 cLostContact				= "AVERTISSEMENT ! Perdu contact radio pour unité de protection à un emplacement inconnu..."
 
 voices						= "Voix"
+
+cmdMeC 						= "Effectuer une action physique dans un rayon proche."
+cmdMeL 						= "Effectuer une action physique dans un grand rayon."
+cmdMeD 						= "Effectuer une action physique directement."
+cmdItC 						= "Effectuer une action autour de soi dans un rayon proche."
+cmdItL 						= "Effectuer une action autour de soi dans un grand rayon."
+cmdItD 						= "Effectuer une action autour de soi directement."
+cmdLocalSound 				= "Reproduire un audio dans le rayon d'action défini."
+
+legs 						= "Jambes"
+optLegsEnabled 				= "Activer l'affichage des jambes"
+optLegsInVehicle 			= "Activer l'affichage des jambes dans les véhicules"
+
+readingBook 				= "En train de lire..."
+
+categoryCard 				= "Cartes d'identification"
+iCardDesc0 					= "Une petite carte métallique avec une bande magnétique utilisée à des fins d'identification, attribuée à %s."
+iCardDesc1 					= "Une petite carte métallique avec une bande magnétique utilisée à des fins d'identification. Elle comporte le logo du Syndicat des Travailleurs Civils avec le nom \"%s\" imprimé dessus."
+iCardDesc2 					= "Une petite carte métallique avec une bande magnétique utilisée à des fins d'identification. Elle comporte le logo de l'Autorité Civile du Cartel avec le nom \"%s\" imprimé dessus."
+iCardDesc3 					= "Une petite carte métallique avec une bande magnétique utilisée à des fins d'identification. Elle comporte le logo de l'Union Universelle avec le nom \"%s\" imprimé dessus."
+cardNotice1 				= "Cette carte est la propriété du Syndicat des Travailleurs Civils. Si vous la trouvez, retournez-la immédiatement à un agent de protection local. Le port illégal constitue une infraction et entraînera des poursuites."
+cardNotice2 				= "Cette carte est la propriété de l'Autorité Civile du Cartel. Si vous la trouvez, retournez-la immédiatement à un agent de protection local. Le port illégal constitue une infraction et entraînera des poursuites."
+cardNotice3 				= "Cette carte est la propriété de l'Union Universelle. Si vous la trouvez, retournez-la immédiatement à un agent de protection local. Le port illégal constitue une infraction et entraînera des poursuites."
+
+scCityAdm 					= "Administration de la Cité"
+scCWU 						= "Syndicat des Travailleurs Civils"
+scOTA 						= "Surveillance Transhumaine"
+
+item.category.crafted 		= "Composants dérivés"
+item.category.raw 			= "Composants de base"
