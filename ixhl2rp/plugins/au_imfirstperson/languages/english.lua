d6045c08e5606b0656766d5b1980788a48fdd528
Schwarz Kruppzo <schwarzkruppzo@gmail.com>
2026-01-25 14:46:34 +0300

    immersive first person: localization

---
+++ ixhl2rp/plugins/au_imfirstperson/languages/english.lua
@@ -0,0 +1,12 @@
+ix.Locale:Build("en")
+
+
+option.category.firstperson 			= "Immersive First Person"
+option.enableImmersiveFirstPerson 		= "Enable Immersive First Person"
+option.enableImmersiveFirstPerson.desc 	= "Enables immersive first person."
+
+option.smoothScale 						= "Smooth Scale"
+option.smoothScale.desc 				= "The scale at which mouse movement will be smoothed."
+
+option.customCrosshair 					= "Enable custom crosshair"
+option.customCrosshair.desc 			= "Replaces the standard crosshair with a custom one."
---
+++ ixhl2rp/plugins/au_imfirstperson/languages/russian.lua
@@ -0,0 +1,12 @@
+ix.Locale:Build("ru")
+
+
+option.category.firstperson 			= "Первое лицо"
+option.enableImmersiveFirstPerson 		= "Включить иммерсивный вид от первого лица"
+option.enableImmersiveFirstPerson.desc 	= "Включает иммерсивный вид от первого лица."
+
+option.smoothScale 						= "Гладкость масштабирования"
+option.smoothScale.desc 				= "Масштаб, с которым будет сглажено движение мыши."
+
+option.customCrosshair 					= "Включить пользовательский прицел"
+option.customCrosshair.desc 			= "Заменяет стандартный прицел на пользовательский."
