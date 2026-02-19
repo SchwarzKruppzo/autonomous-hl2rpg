dd7ac9ddf3a5275d5b5a9bb1254922f00ba1c1e7
Schwarz Kruppzo <schwarzkruppzo@gmail.com>
2026-02-19 12:37:26 +0300

    appearance: updated Female01 face map & split makeup into two layers

--- helix/gamemode/game/character/appearance/sh_chargen.lua
+++ helix/gamemode/game/character/appearance/sh_chargen.lua
@@ -3,7 +3,8 @@ local CharGen = ix.util.Lib("CharGen", {
 		FaceMap = 1,
 		HairScalp = 2,
 		Lips = 3,
-		Makeup = 4
+		Makeup = 4,
+		Makeup2 = 5
 	},
 	
 	_models = {},
@@ -12,6 +13,7 @@ local CharGen = ix.util.Lib("CharGen", {
 CharGen.Atlas = {}
 CharGen.FaceMorph = {}
 CharGen.TextureLayers = {}
+CharGen.Bodygroups = {}
 
 function CharGen:RegisterClass(class)
 	local bundle = {}
@@ -92,6 +94,46 @@ function CharGen:GetOptions(class, option)
 	return bundle[option]
 end
 
+function CharGen:AddBodygroupCategory(class, option, name)
+	local bodygroups = self.Bodygroups[class] or {}
+
+	local category = bodygroups[option] or {}
+	category.id = option
+	category.title = name
+	category.options = category.options or {}
+
+	self.Bodygroups[class] = bodygroups
+	self.Bodygroups[class][option] = category
+end
+
+function CharGen:AddBodygroupOption(class, option, index, name, bodygroups)
+	local bundle = self.Bodygroups[class] or {}
+
+	if !bundle[option] then return end
+	
+	bundle[option].options[index] = {
+		id = index,
+		title = name,
+		bodygroups = bodygroups
+	}
+
+	self.Bodygroups[class] = bundle
+end
+
+function CharGen:GetBodygroupCategories(class)
+	local bodygroups = self.Bodygroups[class] or {}
+
+	return bodygroups
+end
+
+function CharGen:GetBodygroupOptions(class, option)
+	local bundle = self:GetBodygroupCategories(class)
+
+	if !bundle[option] then return end
+	
+	return bundle[option].options or {}
+end
+
 function CharGen:IsValid(class, option, index)
 	local bundle = self.Atlas[class]
 
@@ -116,24 +158,44 @@ function CharGen:GetModelClass(model)
 	return self._models[model:lower()]
 end
 
-CharGen:AddTextureLayer("female", CharGen.Option.HairScalp, 1, "Hair Scalp")
-CharGen:AddTextureLayer("female", CharGen.Option.Lips, 1, "Lipstick")
-CharGen:AddTextureLayer("female", CharGen.Option.Makeup, 1, "Makeup")
-
 CharGen:AddOption("female", CharGen.Option.FaceMap, 1, "Base", "facemap/base")
-CharGen:AddOption("female", CharGen.Option.FaceMap, 2, "Female01", "facemap/female01")
+CharGen:AddOption("female", CharGen.Option.FaceMap, 2, "Female01", "facemap/female01hd")
 CharGen:AddOption("female", CharGen.Option.FaceMap, 3, "Ada Wong", "facemap/wong_hd")
 CharGen:AddOption("female", CharGen.Option.FaceMap, 4, "Firefly", "facemap/firefly2")
 CharGen:AddOption("female", CharGen.Option.FaceMap, 5, "Claire Redfield", "facemap/claire")
 
+CharGen:AddTextureLayer("female", CharGen.Option.HairScalp, 1, "Hair Scalp")
+CharGen:AddTextureLayer("female", CharGen.Option.Lips, 1, "Lipstick")
+CharGen:AddTextureLayer("female", CharGen.Option.Makeup, 1, "Eye Makeup")
+CharGen:AddTextureLayer("female", CharGen.Option.Makeup2, 1, "Face Makeup")
 
-CharGen:AddOption("female", CharGen.Option.HairScalp, 1, "Wong", "scalp/hair_wong")
-CharGen:AddOption("female", CharGen.Option.HairScalp, 2, "Claire", "scalp/hair_claire")
-CharGen:AddOption("female", CharGen.Option.HairScalp, 3, "Firefly", "scalp/hair_firefly")
-CharGen:AddOption("female", CharGen.Option.HairScalp, 4, "Hair Scalp 1", "scalp/hair_black")
+CharGen:AddOption("female", CharGen.Option.HairScalp, 1, "Scalp Wong", "scalp/hair_wong")
+CharGen:AddOption("female", CharGen.Option.HairScalp, 2, "Scalp Claire", "scalp/hair_claire")
+CharGen:AddOption("female", CharGen.Option.HairScalp, 3, "Scalp Firefly", "scalp/hair_firefly")
+CharGen:AddOption("female", CharGen.Option.HairScalp, 4, "Scalp 1", "scalp/hair_black")
 
 CharGen:AddOption("female", CharGen.Option.Lips, 1, "Lipstick 1", "lips/lips1", Color(255, 0, 0))
 CharGen:AddOption("female", CharGen.Option.Makeup, 1, "Makeup 1", "makeup/eyedark")
+CharGen:AddOption("female", CharGen.Option.Makeup, 2, "Makeup 2", "makeup/eyedark2")
+CharGen:AddOption("female", CharGen.Option.Makeup2, 1, "Blush 1", "makeup/redness")
+
+do
+	local BodyCategory = {
+		Hair = 1,
+		Lashes = 2
+	}
+
+	CharGen:AddBodygroupCategory("female", BodyCategory.Hair, "Hair")
+	CharGen:AddBodygroupOption("female", BodyCategory.Hair, 1, "No", { [6] = 6 })
+	CharGen:AddBodygroupOption("female", BodyCategory.Hair, 2, "Hair 1", { [6] = 5 })
+	CharGen:AddBodygroupOption("female", BodyCategory.Hair, 3, "Hair 2", { [6] = 4 })
+	CharGen:AddBodygroupOption("female", BodyCategory.Hair, 4, "Hair 3", { [6] = 0 })
+
+	CharGen:AddBodygroupCategory("female", BodyCategory.Lashes, "Lashes")
+	CharGen:AddBodygroupOption("female", BodyCategory.Lashes, 1, "No", { [7] = 0 })
+	CharGen:AddBodygroupOption("female", BodyCategory.Lashes, 2, "Lashes 1", { [7] = 2 })
+	CharGen:AddBodygroupOption("female", BodyCategory.Lashes, 3, "Lashes 2", { [7] = 1 })
+end
 
 CharGen:AddFaceMorph("female", 1, "Female01", 0, Vector(0.05, 0, 0.125), Vector(0.05, 0, -0.075))
 CharGen:AddFaceMorph("female", 2, "Ada Wong", 1, Vector(-0.01, 0, 0.05), Vector(0, 0, 0))
