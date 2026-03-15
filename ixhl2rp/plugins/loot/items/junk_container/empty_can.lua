f687132005285ebbaa584dfffb9d99c4211bddeb
Schwarz Kruppzo <schwarzkruppzo@gmail.com>
2026-03-15 15:54:00 +0300

    junk: added junk_container - reusable reagent containers

--- ixhl2rp/plugins/loot/items/junk_container/empty_can.lua
+++ ixhl2rp/plugins/loot/items/junk_container/empty_can.lua
@@ -1,8 +1,6 @@
 ITEM.name = "Пустая банка"
 ITEM.description = "Пустая алюминиевая банка."
-ITEM.category = "Хлам"
 ITEM.model = "models/props_junk/popcan01a.mdl"
 ITEM.width = 1
 ITEM.height = 1
-ITEM.max_stack = 4
-ITEM.stackable = true
+ITEM.volume = 330
--- ixhl2rp/plugins/loot/items/junk_container/empty_glass_bottle.lua
+++ ixhl2rp/plugins/loot/items/junk_container/empty_glass_bottle.lua
@@ -1,8 +1,6 @@
 ITEM.name = "Пустая стеклянная бутылка"
 ITEM.description = "Пустая стеклянная бутылка, похоже, что из под какого-то алкоголя."
-ITEM.category = "Хлам"
 ITEM.model = "models/props_junk/garbage_glassbottle003a.mdl"
 ITEM.width = 1
 ITEM.height = 2
-ITEM.max_stack = 4
-ITEM.stackable = true
+ITEM.volume = 500
