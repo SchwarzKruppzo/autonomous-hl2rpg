21e1af761d990a35139224a31935029e1f5cf639
Schwarz Kruppzo <schwarzkruppzo@gmail.com>
2026-04-04 14:12:00 +0300

    glassware: update liquid visual in inventory

--- helix/plugins/reagents/items/base/glass_container.lua
+++ helix/plugins/reagents/items/base/glass_container.lua
@@ -96,6 +96,9 @@ function ItemGlass:Init()
 
 			if IsValid(entity) then
 				entity.targetLiquidFrac = value / self:GetMaxVolume()
+			else
+				item.cachedFluidColor = nil
+				item.updateFluidVisual = true
 			end
 		end)
 	end
@@ -134,8 +137,8 @@ if CLIENT then
 		end
 	end
 
-	local function SetLiquidLevel(entity, level)
-		local info = entity:GetItem()
+	local function SetLiquidLevel(entity, level, info)
+		info = info or entity:GetItem()
 
 		entity.liquid_offset = entity.liquid_offset or {}
 		entity.liquid_scale = entity.liquid_scale or {}
@@ -178,20 +181,52 @@ if CLIENT then
 		if entity.liquidFrac != entity.targetLiquidFrac then
 			entity.liquidFrac = Lerp(10 * FrameTime(), entity.liquidFrac, entity.targetLiquidFrac or entity.liquidFrac)
 
-			SetLiquidLevel(entity, entity.liquidFrac)
+			SetLiquidLevel(entity, entity.liquidFrac, entity.liquidInfo)
 		end
 	end
 
-	function ItemGlass:DrawEntity(entity)
+	function ItemGlass:DrawEntity(entity, info)
 		if !entity.liquidReady then
 			entity.liquidReady = true
 
 			entity.liquidFrac = (self:GetData("value") or 0) / self:GetMaxVolume()
 			entity.targetLiquidFrac = entity.liquidFrac
 
-			entity.Think = EntityThink
+			if info then
+				entity.liquidInfo = info
+			else
+				entity.Think = EntityThink
+			end
 
-			SetLiquidLevel(entity, entity.liquidFrac)
+			SetLiquidLevel(entity, entity.liquidFrac, info)
+
+			self.updateFluidVisual = true
+		end
+	end
+
+	function ItemGlass:LayoutIcon(panel, entity)
+		self:DrawEntity(entity, self)
+
+		if self.updateFluidVisual then
+			local updateColor = self:GetReagentsColor()
+
+			entity.liquid_color = updateColor
+			entity.targetLiquidFrac = (self:GetData("value") or 0) / self:GetMaxVolume()
+
+			self.updateFluidVisual = false
+		end
+
+		EntityThink(entity)
+	end
+
+	function ItemGlass:PopulateTooltip(tooltip)
+		if !self:GetEntity() then
+			local currentVolume = math.Round(self:GetVolume())
+			local maxVolume = self:GetMaxVolume()
+
+			local vol = tooltip:AddRowAfter("name")
+			vol:SetBackgroundColor(derma.GetColor("Success", tooltip))
+			vol:SetText(L("volumeDesc", currentVolume, maxVolume))
 		end
 	end
 end
